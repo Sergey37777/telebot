01b06f839e3c131908a7495c5ae58ce4824ef94f
sergejlapin <sergey.l37@icloud.com>
2024-03-21 22:30:04 +0300

    Added reply keyboard

---
+++ kbds/reply.py
@@ -0,0 +1,15 @@
+from typing import List, Tuple
+
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, ReplyKeyboardMarkup, KeyboardButton
+
+
+def get_admin_keyboard(*buttons, requests_contact: None | List[int], sizes: Tuple[int, ...]) -> ReplyKeyboardMarkup:
+    builder = ReplyKeyboardBuilder()
+    for idx, text in enumerate(buttons):
+        if requests_contact and idx in requests_contact:
+            builder.add(KeyboardButton(text=text, request_contact=True))
+        else:
+            builder.add(KeyboardButton(text=text))
+    builder.adjust(*sizes)
+    return builder.as_markup()
+
