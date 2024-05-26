ed45de4fd4bcf7f11cd5436ca3065f921ce26f80
sergejlapin <sergey.l37@icloud.com>
2024-05-26 17:43:01 +0300

    Added create_db; drop_db

--- database/engine.py
+++ database/engine.py
@@ -1,6 +1,19 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
-
-
+from database.models import Base
 engine = create_async_engine(f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}", echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+async def create_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    """async with async_session() as session:
+        await orm_create_categories(session, categories)
+        await orm_add_banner_description(session, description_for_info_pages)"""
+
+
+async def drop_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
