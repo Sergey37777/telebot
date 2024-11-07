fb8a85df5b6ecafd328a1adc3433d37da585e635
sergejlapin <sergey.l37@icloud.com>
2024-11-07 22:30:01 +0300

    added add_to_cart func

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -3,6 +3,7 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from database.orm_queries import orm_add_user, orm_add_to_cart, orm_check_user
 from filters.ChatTypeFilter import ChatTypeFilter
 from handlers.menu_processing import get_menu_content
 from kbds.inline import get_customer_keyboard, get_callback_buttons, MenuCallBack
@@ -31,15 +32,28 @@ async def show_catalog(query: CallbackQuery):
     )"""
 
 
+async def add_to_cart(query: CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
+    user = query.from_user
+    if not await orm_check_user(session, user.id):
+        await orm_add_user(session, user.id, user.first_name, user.last_name)
+    await orm_add_to_cart(session, user.id, callback_data.product_id)
+    await query.answer('Товар добавлен в корзину')
+
+
 @user_router.callback_query(MenuCallBack.filter())
 async def show_catalog(query: CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
     # print(query.data)
     # print(callback_data)
+    if callback_data.menu_name == 'add_to_cart':
+        await add_to_cart(query, callback_data, session)
+        return
     media, reply_markup = await get_menu_content(session,
                                                  level=callback_data.level,
                                                  menu_name=callback_data.menu_name,
                                                  category=callback_data.category,
-                                                 page=callback_data.page)
+                                                 page=callback_data.page,
+                                                 product_id=callback_data.product_id,
+                                                 user_id=query.from_user.id)
     # await query.message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
     await query.message.edit_media(media, reply_markup=reply_markup)
     await query.answer()
